516223aa1dd62c8571541c0b2b9dd5ab7dc6b5c9
AliCheves <20245542@esen.edu.sv>
2026-02-08 20:43:19 -0600

    ReUpload

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { db } from './data/db'
+import Guitar from './components/Guitar'
+
+export default function App() {
+
+  function initialCart(){
+    const localStorageCart = localStorage.getItem('cart');
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  }
+
+  const [data, setData] = useState(db)
+  const [cart, setCart] = useState(initialCart())
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+
+  function addToCart(guitar){
+    const itemIndex=cart.findIndex((item)=>guitar.id===item.id)
+    console.log(itemIndex);
+    if(itemIndex===-1){
+      guitar.quantity=1;
+      setCart([...cart,guitar ])
+    }
+    else{
+      const updatedCart=[...cart] 
+      updatedCart[itemIndex].quantity++;
+      setCart(updatedCart);
+    }
+  }
+  
+  function calculateTotal(cart){
+    // let total=0;
+    // for (const guitar of cart){
+    //   total+=guitar.price*guitar.quantity;
+    // }
+    let total=cart.reduce((total, item) => total + item.price * item.quantity, 0);
+    return total;
+  }
+
+  function increaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id){
+        return {...item, quantity: item.quantity + 1}
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id){
+    const updatedCart = cart.map(item => {
+      if(item.id === id && item.quantity > 1){
+        return {...item, quantity: item.quantity - 1}
+      }
+      return item;
+    });
+    setCart(updatedCart);
+  }
+
+  function removeFromCart(id){
+    const updatedCart = cart.filter(item => item.id !== id);
+    setCart(updatedCart);
+  }
+
+  function clearCart(){
+    setCart([]);
+  }
+
+
+
+  return (
+    <>
+      <Header 
+        cart={cart} 
+        total={calculateTotal(cart)}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        removeFromCart={removeFromCart}
+        clearCart={clearCart}
+      />
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+        <div className="row mt-5">
+          {data.map((guitar)=>(
+            <Guitar guitar={guitar} key={guitar.id} addToCart={addToCart} />
+          ))}
+          
+          
+        </div>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
